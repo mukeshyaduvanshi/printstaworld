75fcc8a942d637974979b989a21bd369d5e1a2a8
myaduvanshi <mmukesh1508@gmail.com>
2025-03-15 01:57:49 +0530

    New SetUp

---
+++ app/(default)/page.tsx
@@ -0,0 +1,18 @@
+// import ComponentsDashboardSales from '@/components/dashboard/components-dashboard-sales';
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+    title: 'Sales Admin',
+};
+
+const Sales = () => {
+    // return <ComponentsDashboardSales />;
+    return(
+        <>
+        <h1>This Page is under construction</h1>
+        </>
+    )
+};
+
+export default Sales;
---
+++ components/layouts/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+            <div className="p-6 pt-0 mt-auto text-center dark:text-white-dark sm:ltr:text-left sm:rtl:text-right">© {new Date().getFullYear()}. Vristo All rights reserved.</div>
+    );
+};
+
+export default Footer;
