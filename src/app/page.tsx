e1a19c10bb4970875a3b88912d746b2a32af542f
myaduvanshi <mmukesh1508@gmail.com>
2025-03-14 22:55:21 +0530

    the theme is setup in a blue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <Button variant={"outline"}>Click me</Button>
+     <Button className="rounded-full btn btn-primary" >Click me</Button>
      <ThemeToggle/>
      <CalendarDemo />
-     <CardWithForm />
+     {/* <CardWithForm /> */}
     </div>
   );
 }
